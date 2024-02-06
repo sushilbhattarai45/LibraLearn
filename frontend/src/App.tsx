acfe82dbef3ce8342b73db40fe5abf53aee08ae6
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2024-02-06 18:15:41 +0545

    Image Gone Wrong

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,40 @@ import Navbar from "./components/fotter";
 
 function App() {
   return (
-    <div>
+    <div
+      style={{
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <Navbar />
+      <div
+        style={{
+          width: "80%",
+          flex: 0.2,
+          height: "100px",
+          display: "flex",
+          alignItems: "center",
+          alignSelf: "center",
+        }}
+      >
+        <img
+          style={{ width: "80%", height: "30%" }}
+          className="h-auto max-w-full"
+          src="https://sushilbhattarai.info.np/static/media/4.d5497b9e379b3895080b.jpg"
+          alt="image description"
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flex: 1,
+        }}
+      >
+        <p>Hello</p>
+      </div>
     </div>
   );
 }
