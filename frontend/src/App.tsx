5676a961b8ef7938aaf6ff852007c9c80fd537ee
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2024-02-07 18:50:36 +0545

    vertical about bar added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,46 @@
 import Navbar from "./components/NavBar";
-
+import VerticalAbout from "./components/About";
 function App() {
-    return (
-        <div>
-            <Navbar />
-            <img
-                className="h-max-[30vh] h-[30vh]"
-                src="https://sushilbhattarai.info.np/static/media/4.d5497b9e379b3895080b.jpg"
-                alt="image description"
-            />
+  return (
+    <div>
+      <Navbar />
+      <div
+        style={{
+          display: "flex",
+          flex: 1,
+          width: "100vw",
+          height: "auto",
+          justifyContent: "center",
+          flexDirection: "row",
+        }}
+      >
+        <div
+          style={{
+            flex: 0.7,
+          }}
+        >
+          <img
+            className="h-max-[30vh] h-[30vh]"
+            src="https://sushilbhattarai.info.np/static/media/4.d5497b9e379b3895080b.jpg"
+            alt="image description"
+          />
         </div>
-    );
-};
+        <div
+          style={{
+            // position: "absolute",
+            // top: "20vh",
+            // height: "10vh",
+            // right: "20vh",
+            // width: "40vh",
+            flex: 0.1,
+          }}
+        >
+          {" "}
+          <VerticalAbout />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default App;
---
+++ frontend/src/components/About.tsx
@@ -0,0 +1,124 @@
+const verticalAbout = () => {
+  return (
+    <>
+      <div className="p-5 mb-4 border border-gray-100 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+        <span
+          style={{
+            textAlign: "center",
+            alignSelf: "center",
+            width: "100%",
+            justifyContent: "center",
+          }}
+          className="text-lg font-semibold text-gray-900 dark:text-white"
+        >
+          <p>Our Admistration </p>
+        </span>
+        <ol className="mt-3 divide-y divider-gray-200 dark:divide-black-700">
+          <li>
+            <a
+              href="#"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+              className="items-center block p-3 sm:flex hover:bg-gray-100 dark:hover:bg-gray-700"
+            >
+              <img
+                style={{
+                  height: "100px",
+                  width: "100px",
+                  borderRadius: "20%",
+                }}
+                className="w-32 h-32 mb-3 me-3 rounded-20 sm:mb-0"
+                src="https://sushilbhattarai.info.np/static/media/4.d5497b9e379b3895080b.jpg"
+                alt="Jese Leos image"
+              />
+              <div
+                style={{
+                  marginTop: 10,
+                }}
+                className="text-gray-600 dark:text-gray-400"
+              >
+                <div
+                  style={{
+                    alignSelf: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                  }}
+                  className="text-base font-normal"
+                >
+                  <span
+                    style={{ textAlign: "center" }}
+                    className="font-medium text-gray-900 dark:text-white"
+                  >
+                    Sushil Bhattarai
+                  </span>{" "}
+                  <br />
+                  <span className="font-medium text-gray-900 dark:text-white">
+                    {" "}
+                    Principle
+                  </span>
+                </div>
+                <div className="text-sm font-normal">
+                  "bhattaraisushil005@gmail.com"
+                </div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+              className="items-center block p-3 sm:flex hover:bg-gray-100 dark:hover:bg-gray-700"
+            >
+              <img
+                style={{
+                  height: "100px",
+                  width: "100px",
+                  borderRadius: "20%",
+                }}
+                className="w-32 h-32 mb-3 me-3 rounded-20 sm:mb-0"
+                src="https://sushilbhattarai.info.np/static/media/4.d5497b9e379b3895080b.jpg"
+                alt="Jese Leos image"
+              />
+              <div
+                style={{
+                  marginTop: 10,
+                }}
+                className="text-gray-600 dark:text-gray-400"
+              >
+                <div
+                  style={{
+                    alignSelf: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                  }}
+                  className="text-base font-normal"
+                >
+                  <span
+                    style={{ textAlign: "center" }}
+                    className="font-medium text-gray-900 dark:text-white"
+                  >
+                    Sushil Bhattarai
+                  </span>{" "}
+                  <br />
+                  <span className="font-medium text-gray-900 dark:text-white">
+                    {" "}
+                    Principle
+                  </span>
+                </div>
+                <div className="text-sm font-normal">
+                  "bhattaraisushil005@gmail.com"
+                </div>
+              </div>
+            </a>
+          </li>
+        </ol>
+      </div>
+    </>
+  );
+};
+export default verticalAbout;
